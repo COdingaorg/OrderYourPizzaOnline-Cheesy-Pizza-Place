5acd896dd64c08c3a86415f455940a508fc3c2d9
Caleb Odinga <calemasanga@gmail.com>
2021-04-25 11:48:11 +0300

    makes yes a reset trigger, separates submit button, with checkout, rectifies topping input

--- JS/scripts.js
+++ JS/scripts.js
@@ -16,31 +16,32 @@ function Topping(name, price) {
   this.Price = price;
 }
 //construct to create pizza objects
-function Pizza  (size, price) {
+function Pizza(size, price) {
   this.Size = size;
   this.Price = price;
   this.Toppings = [];
   this.Crusts = [];
+  this.Total= 0;
 }
 //get total price for a selected pizza
- Pizza.prototype.pizzaTotal = function(){
+Pizza.prototype.pizzaTotal = function () {
   return (this.Price + this.Toppings[0].Price + this.Crusts[0].Price)
 }
- //get crust type
- Pizza.prototype.pizzaCrust = function(){
-   return (this.Crusts[0].Type)
- }
- //get topping chosen
- Pizza.prototype.pizzaTopping = function(){
-   return (this.Toppings[0].Name)
- }
+//get crust type
+Pizza.prototype.pizzaCrust = function () {
+  return (this.Crusts[0].Type)
+}
+//get topping chosen
+Pizza.prototype.pizzaTopping = function () {
+  return (this.Toppings[0].Name)
+}
 //user Logic
-$(document).ready(function(){
-  $('form#pizzaorder').submit(function(event){
+$(document).ready(function () {
+  $('form#pizzaorder').submit(function (event) {
     event.preventDefault();
     //Collecting input from form
     var pizzasize = $('select#size').val();
-    var pizzatopping = $('input#topping').val();
+    var pizzatopping = $('input[name="topping"]:checked').val();
     var pizzacrust = $('select#crust').val();
     //determine crust price
     var crustPrice = function () {
@@ -80,60 +81,64 @@ $(document).ready(function(){
     }
     var pizzaSelect = new Pizza(pizzasize, pizzaPrice());
 
-   //add toppings and crust objects to Pizza object
+    //add toppings and crust objects to Pizza object
     pizzaSelect.Toppings.push(toppingSelect);
     pizzaSelect.Crusts.push(crustSelect);
-    
-   
-     
-         
+    pizzaSelect.Total=(pizzaSelect.pizzaTotal())
+
+
+
+
     //show order to HTML
-    if(pizzaSelect.Size=='select size'||pizzSelect.pizzaCrust()=='Choose crust type'){
+    if (pizzaSelect.Size == 'select size' || pizzaSelect.pizzaCrust() == 'Choose crust type') {
       alert('Select a valid option');
     }
-      $('#orderSummary').append('<div class="summaries"><h5>Pizza Selected : </h5><p>' +pizzaSelect.Size+ '</p>' +
-                                '<h5>Toppings: </h5><p>' +pizzapizzaSelect.pizzaTopping()+ '</p>' +
-                                '<h5>Crust Type: </h5><p>' +pizzSelect.pizzaCrust()+ '</p>' +
-                                '<h4>SUB-TOTAL</h4>'+pizzaSelect.pizzaTotal()+'</div>')
+    $('#orderSummary').append('<div class="summaries"><h5>Pizza Selected : </h5><p>' + pizzaSelect.Size + '</p>' +
+      '<h5>Toppings: </h5><p>' + pizzaSelect.pizzaTopping() + '</p>' +
+      '<h5>Crust Type: </h5><p>' + pizzaSelect.pizzaCrust() + '</p>' +
+      '<h4>SUB-TOTAL</h4>' + pizzaSelect.pizzaTotal() + '</div>')
     //show popup for adding another order
     $('#addPopup').show();
-    $('#anotherOne').click(function(){
+    $('#anotherOne').click(function () {
       var anotherAns = $("input[name='anotherOrder']:checked").val();
-      if(anotherAns == 'addYes'){
+      if (anotherAns == 'addYes') {
         $('#addPopup').hide();
-      }else if(anotherAns =='addNo'){
+        $('#pizzaorder').trigger('reset')
+      } else if (anotherAns == 'addNo') {
         $('#addPopup').hide();
         $('#selectDelivery').show();
-      }else($('.error').show())
+      } else ($('.error').show())
     })
-  $('#checkout').click(function(event){  
-    event.preventDefault();                     
-    //calculate delivery
-    var delivery = $('input[name="delivery"]:checked').val();
-    var deliveryPrice = function(){
-      if(delivery == 'delivery'){
-        return 350
-      }else if(delivery == 'no-delivery'){
-        $('.location').hide()
-        return 0
-      }
-    }
-    //incase of multiple pizza orders, find a total price for all pizzas
-    var allPizzaTotal = pizzaSelect.each(function(){
-
-    })
-    var totalAmount = deliveryPrice()+(pizzaSelect.pizzaTotal());
-    var location = $('input#location').val();
-    //checking if location is added
-    if(delivery=='delivery'&&(location==null||location=='')){
-      alert('Please enter a location')
-    };
-    //display total at checkout
-      $('#deliveryDetails').append('<p>Your Delivery will be done to : '+'<h3>'+location+'</h3>in 30 minutes</p>')
-      $('#totalAmount').append('<p> Delivery cost : '+deliveryPrice()+'</p>'+'<h1>TOTAL AMOUNT :'+totalAmount+'</h1>'+
-                                '<p>Thank you for shopping with us..</p>')
+  })
+    $('#checkout').click(function (event) {
+      event.preventDefault();
+      //calculate delivery
+      var delivery = $('input[name="delivery"]:checked').val();
+      var deliveryPrice = function () {
+        if (delivery == 'delivery') {
+          return 350
+        } else if (delivery == 'no-delivery') {
+          $('.location').hide()
+          return 0
+        }
+      } 
+      //incase of multiple pizza orders, find a total price for all pizzas
+      
+      var totalAmount = 0;
+      var location = $('input#location').val();
+      //checking if location is added
+      if (delivery == 'delivery' && (location == null || location == '')) {
+        alert('Please enter a location')
+      };
+      //display total at checkout
+      $('#deliveryDetails').append('<p>Your Delivery will be done to : ' + '<h3>' + location +
+                                   '</h3>in 30 minutes</p>'+
+                                   '<p> Delivery cost : ' + deliveryPrice()+ '</p>' )
+      $('#totalAmount').append('<h1>TOTAL AMOUNT :' +totalAmount+ '</h1>')
+      console.log(totalAmount)
       $('#checkout').hide();
-      $('#seeOrder').show();                  
-  })    
-  }) 
+      $('#seeOrder').show();
+      $('.thankyou').show();
+    })
+  
 })
